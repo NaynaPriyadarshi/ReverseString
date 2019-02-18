56357fd1e034c0b10cf575daa68c6e36febb4891
NaynaPriyadarshi <you@example.com>
2019-02-18 16:39:40 +1000

    Add project files.

---
+++ Program_ReverseString1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_ReverseString1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Word = "a b c d e f";
+            for(int i= Word.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(Word[i] + "");
+
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
